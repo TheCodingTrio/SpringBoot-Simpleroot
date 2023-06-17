fe462c1c2f1ed725c6149447d84226beb267fced
IT21440236 <it21440236@my.sliit.lk>
2023-06-17 11:21:05 +0530

    push to main 1

--- src/main/java/com/example/simpleroot/service/UserService.java
+++ src/main/java/com/example/simpleroot/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.simpleroot.service;
 
 import com.example.simpleroot.dto.UserDTO;
+import com.example.simpleroot.entity.User;
 import com.example.simpleroot.repo.UserRepo;
 import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +17,20 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     //data is taken through the DTO
-    public String saveUser(UserDTO userDTO){
-         userRepo.save(userDTO);
+    public UserDTO saveUser(UserDTO userDTO){
+         userRepo.save(modelMapper.map(userDTO, User.class));
          //We save the data via the entity, but we get the data via the DTO.
         //So we have to Map these two, the entity and the DTO
         //To do this we need to add the modelmapper dependency
         //after add the dependency we need to inject the modelmapper to our project as a @Bean
         //that happens in the class where we have the main method (CHECK!!)
+        //after that we can inject the class as done above using @Autowired
+
+        return userDTO;
     }
 }
